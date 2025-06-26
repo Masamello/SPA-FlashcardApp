97d3b0baa9c625ed8711d95a861b4ae62755a41e
Masamello <kojimasa1205@gmail.com>
2025-06-25 21:00:53 -0700

    Update App.js

---
+++ flashcard-app/src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './Context/AuthContext';
+import { FlashcardProvider } from './Context/FlashcardContext';
+import Navbar from './components/Navbar';
+import Home from './Pages/Home';
+import Dashboard from './Pages/Dashboard';
+import Details from './Pages/Details';
+import Profile from './Pages/Profile';
+import './App.css';
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <FlashcardProvider>
+        <Router>
+          <Navbar />
+          <div className="container py-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/flashcards/:id" element={<Details />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+          </div>
+        </Router>
+      </FlashcardProvider>
+    </AuthProvider>
+  );
+};
+
+export default App;
